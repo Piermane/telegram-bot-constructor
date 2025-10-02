5fc880dc68294d2fe1f424bbf7150950dc5f9361
Piermane <bojkerushqa@gmail.com>
2025-10-02 19:18:20 +0300

    UPDATE

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,21 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Box, Flex } from '@chakra-ui/react';
 import Sidebar from './Sidebar';
 import Header from './Header';
 
 const Layout: React.FC = () => {
+  useEffect(() => {
+    const interBubble = document.querySelector<HTMLDivElement>('.interactive');
+    if (!interBubble) return;
+    
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+      curX += (tgX - curX) / 20;
+      curY += (tgY - curY) / 20;
+      if (interBubble) {
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+      }
+      requestAnimationFrame(move);
+    }
+
+    const handleMouseMove = (event: MouseEvent) => {
+      tgX = event.clientX;
+      tgY = event.clientY;
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    move();
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <>
-      {/* Diagonal mesh background (fixed, behind everything) */}
-      <div className="mesh-bg">
-        <div className="mesh-blob mesh-blob-1"></div>
-        <div className="mesh-blob mesh-blob-2"></div>
-        <div className="mesh-blob mesh-blob-3"></div>
-        <div className="mesh-blob mesh-blob-4"></div>
+      {/* Gradient background from gradients-bg-main */}
+      <div className="gradient-bg">
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
+        <div className="gradients-container">
+          <div className="g1"></div>
+          <div className="g2"></div>
+          <div className="g3"></div>
+          <div className="g4"></div>
+          <div className="g5"></div>
+          <div className="interactive"></div>
+        </div>
       </div>
 
-      <Flex minH="100vh" position="relative" style={{ zIndex: 1 }}>
+      <Flex minH="100vh" position="relative" style={{ zIndex: 100 }}>
         <Sidebar />
         <Box flex="1" ml={{ base: 0, md: '250px' }}>
           <Header />
